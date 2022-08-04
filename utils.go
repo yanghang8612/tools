1290641ad354d90a89cb90a044d715b29f997d46
jeancky <jeancky@163.com>
2022-08-04 18:29:35 +0800

    Update utils.go

--- utils.go
+++ utils.go
@@ -3,11 +3,11 @@ package main
 import "strings"
 
 func dropHexPrefix(str string) string {
-	if strings.HasPrefix(str, "x") || strings.HasPrefix(str, "X") {
-		str = str[1:]
-	}
+	
 	if strings.HasPrefix(str, "0x") || strings.HasPrefix(str, "0X") {
 		str = str[2:]
+	}else if strings.HasPrefix(str, "x") || strings.HasPrefix(str, "X") {
+		str = str[1:]
 	}
 	return str
 }
