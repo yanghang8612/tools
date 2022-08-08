6aa5fa7a1303825918b67855b601ad0008750135
Asuka <yanghang8612@163.com>
2022-08-08 10:07:56 +0800

    func(root,time): support more arbitrary input

--- main.go
+++ main.go
@@ -33,12 +33,19 @@ var (
 				} else {
 					// input str is date or time
 					loc, _ := time.LoadLocation("Asia/Shanghai")
-					formats := []string{"2006-01-02 15:04:05", "2006-01-02 15:04", "2006-01-02 15", "2006-01-02",
-						"01-02 15:04:05", "01-02 15:04", "01-02", "15:04:05", "15:04"}
+					formatsWithDateButNoYear := []string{"1-2 15:4:5", "1-2 15:4", "1-2 15", "1-2"}
+					formatsWithoutDate := []string{"15:4:5", "15:4"}
+					formats := formatsWithDateButNoYear
+					// append two kinds of year to formats
+					for _, f := range formatsWithDateButNoYear {
+						formats = append(formats, "2006-"+f)
+						formats = append(formats, "06-"+f)
+					}
+					formats = append(formats, formatsWithoutDate...)
 					for _, format := range formats {
 						if dt, err := time.ParseInLocation(format, arg, loc); err == nil {
 							if !strings.ContainsAny(format, "-") {
-								dt = dt.AddDate(time.Now().Year(), int(time.Now().Month())-1, time.Now().Day())
+								dt = dt.AddDate(time.Now().Year(), int(time.Now().Month())-1, time.Now().Day()-1)
 							}
 							if dt.Year() == 0 {
 								dt = dt.AddDate(time.Now().Year(), 0, 0)
